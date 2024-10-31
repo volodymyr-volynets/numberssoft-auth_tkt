97a7cb5665ea6e402a7ae7a60e64de74778cf035
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2024-10-30 23:31:57 -0400

    Added md5 implementation

--- test/auth_tkt.test.js
+++ test/auth_tkt.test.js
@@ -6,9 +6,13 @@ describe('AuthTkt', function () {
   it('Create new token and then validate', function () {
     const auth_tkt = new AuthTkt();
     auth_tkt.setOptions({
-      "token_key": "123456"
+      "token_key": "123456",
+      "check_ip": false
     });
-    const token = auth_tkt.tokenCreate(3, 'test_token', {"user_id": 200});
+    const token = auth_tkt.tokenCreate(3, 'test_token', {"user_id": 200}, {
+      time: 123456,
+      ip: '127.0.0.1'
+    });
     //console.log('Token:', token);
     assert.notEqual(token, null);
     const validated = auth_tkt.tokenValidate(decode(token));
